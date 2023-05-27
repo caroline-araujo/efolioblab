602f38fa3f40fdae746e0bcfd45c67ac55d07885
Caroline Araujo <carolinearaujo570@gmail.com>
2023-05-27 17:59:48 +0100

    alterando display dos retângulos

--- main.c
+++ main.c
@@ -98,13 +98,16 @@ float calculateInstability(Rectangle *rectangles, int numRectangles) {
 void displayRectangles(Rectangle *rectangles, int numRectangles) {
     char display[25][80] = {0}; // Matriz para representar o plano
 
-    // Preencher a matriz com os retângulos
+    // Preencher as bordas dos retângulos na matriz
     for (int i = 0; i < numRectangles; i++) {
         Rectangle *rect = &rectangles[i];
+        for (int j = rect->x; j < rect->x + rect->l; j++) {
+            display[rect->y][j] = 'x';
+            display[rect->y + rect->h - 1][j] = 'x';
+        }
         for (int j = rect->y; j < rect->y + rect->h; j++) {
-            for (int k = rect->x; k < rect->x + rect->l; k++) {
-                display[j][k] = 'x';
-            }
+            display[j][rect->x] = 'x';
+            display[j][rect->x + rect->l - 1] = 'x';
         }
     }
 
@@ -114,7 +117,7 @@ void displayRectangles(Rectangle *rectangles, int numRectangles) {
     // Exibir o plano no console com o grau de instabilidade
     for (int i = 0; i < 25; i++) {
         for (int j = 0; j < 80; j++) {
-            if (display[i][j] == 'x') {
+            if (display[i][j] == 'x' || display[i][j] == 'x') {
                 printf("%c", display[i][j]);
             } else {
                 if (instabilityDegree > 0) {
@@ -128,6 +131,7 @@ void displayRectangles(Rectangle *rectangles, int numRectangles) {
         printf("\n");
     }
 }
+
 
 int main() {
     Rectangle rectangles[MAX_RECTANGLES];
